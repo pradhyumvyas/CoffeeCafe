e34bc2c5c4b1347063431d1a56220e0f5c9857c8
pradhyumvyas <pradhyumvyas92@gmail.com>
2020-04-25 02:17:20 +0530

    Adding Navbar

---
+++ js/loader.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.modal').modal();
+    $('.parallax').parallax();
+    $('.sidenav').sidenav();
+})
+
+function toggleModal(){
+    var instance = M.Modal.getInstance($('#modal3'))
+    instance.open()
+}
