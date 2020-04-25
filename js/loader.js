494acb798224694b126e726668690e40e4674791
pradhyumvyas <pradhyumvyas92@gmail.com>
2020-04-26 03:53:51 +0530

    Added Reviews Section

--- js/loader.js
+++ js/loader.js
@@ -4,6 +4,11 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
     $(".slider").slider({ full_width: true });
     $('.parallax').parallax();
+    $('.myreviews').carousel({
+        numVisible: 7,
+        shift:55,
+        padding:55
+    })
 })
 
 function toggleModal(){
