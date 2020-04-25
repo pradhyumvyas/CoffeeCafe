abe6b6a5c3d9745517e255fb7d8a9056f5f6879a
pradhyumvyas <pradhyumvyas92@gmail.com>
2020-04-25 17:54:43 +0530

    Adding Slider

--- js/loader.js
+++ js/loader.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $('.modal').modal();
     $('.parallax').parallax();
     $('.sidenav').sidenav();
+    $(".slider").slider({ full_width: true });
 })
 
 function toggleModal(){
